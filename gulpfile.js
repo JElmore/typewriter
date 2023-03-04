5bea448ad4b15494016d228a913202aea63502d5
Jay Elmore <jelmore49@gmail.com>
2023-03-03 16:02:19 -0800

    Move source files into src directory, update .gitignore and gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,43 @@ const gsass        = require('gulp-sass')(require('sass'));
 const zip          = require('gulp-zip');
 
 function sass(cb) {
-  gulp.src('./assets/sass/*.scss')
+  gulp.src('./src/assets/sass/*.scss')
   .pipe(gsass())
   .pipe(autoprefixer())
   .pipe(rename({suffix: '.min'}))
   .pipe(cleancss())
-  .pipe(gulp.dest('./assets/css'));
+  .pipe(gulp.dest('./build/assets/css'));
+  cb();
+}
+
+function collect(cb) {
+  gulp.src('./src/assets/js/*.js')
+  .pipe(gulp.dest('./build/assets/js'));
+  gulp.src('./src/partials/*.hbs')
+  .pipe(gulp.dest('./build/partials'));
+  gulp.src('./src/*.hbs')
+  .pipe(gulp.dest('./build'));
+  gulp.src('./package.json')
+  .pipe(gulp.dest('./build'));
+  gulp.src('./README.md')
+  .pipe(gulp.dest('./build'));
+  gulp.src('./LICENSE')
+  .pipe(gulp.dest('./build'));
   cb();
 }
 
 function watch(cb) {
-    gulp.watch('./assets/sass/**/*.scss', sass);
+    gulp.watch('./src/assets/sass/*.scss', sass);
     cb();
   }
 
 function package(cb) {
-  gulp.src('./**/*')
+  gulp.src('./build/**/*')
   .pipe(zip('typewriter.zip'))
-  .pipe(gulp.dest('..'));
+  .pipe(gulp.dest('./package'));
   cb();
 }
 
-exports.build = sass
+exports.build   = gulp.series(sass, collect)
 exports.default = watch
-exports.package = gulp.series(sass, package)
+exports.package = gulp.series(sass, collect, package)
